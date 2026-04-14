ebf9102f6e28ac3da4a12ecc60aa9dac024cd0d6
Nithishkumar <rnithishkumar080417@gmail.com>
2026-04-14 07:55:18 +0530

    added visual testing

---
+++ tests/MoreValidations.spec.js
@@ -0,0 +1,27 @@
+const {test,expect}=require("@playwright/test");
+const path = require("node:path");
+
+test("Popup validation",async({page})=>{
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+    await expect(page.locator("#displayed-text")).toBeVisible();
+    await page.locator("#hide-textbox").click();
+    await expect(page.locator("#displayed-text")).toBeHidden();
+    await page.locator("#confirmbtn").click();
+    page.on("dialog",dialog=>dialog.accept());
+    await page.locator("#mousehover").hover();
+    const framePage=page.frameLocator("#courses-iframe");
+    await framePage.locator("li a[href$='lifetime-access']:visible").click();
+});
+
+test("ScreenShot",async({page})=>{
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+    await expect(page.locator("#displayed-text")).toBeVisible();
+    await page.locator("#displayed-text").screenshot({path:"test-results/screenshots/Partial.jpg"});
+    await page.locator("#hide-textbox").click();
+    await page.screenshot({path:"test-results/screenshots/WholeScreenShot.jpg"});
+});
+
+test.only("Visual testing",async({page})=>{
+    await page.goto("https://dashboard.fastpix.co/login");
+    expect(await page.screenshot()).toMatchSnapshot("ProdLandingPage");
+});
