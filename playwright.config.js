4af1edcb65fe30f0349a2eee656b731a95e124df
Nithishkumar <nithiskumar.r2021cseb@sece.ai.in>
2026-04-19 12:11:40 +0530

    Implemented POM

--- playwright.config.js
+++ playwright.config.js
@@ -1,21 +1,18 @@
 import { chromium, defineConfig, devices } from '@playwright/test';
 import { trace } from 'node:console';
 
-/**
- * @see https://playwright.dev/docs/test-configuration
- */
 const config=({
   testDir: './tests',
   timeout:30*1000,
   expect:{
     timeout:30000
   },
-  //reporter:'html',
+  reporter:'html',
   use:{
     browserName:'chromium',
     headless: true,
     screenshot:"on",
-    //trace:"retain-on-failure",//on,off,retain-on-failure
+    trace:"retain-on-failure",//on,off,retain-on-failure
   },
 });
 
